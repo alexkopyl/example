6bc345f337447428880ddf2d6a370adddf2352b6
quark <>
2022-02-15 18:59:35 +0500

    all

---
+++ src/one/One.java
@@ -0,0 +1,13 @@
+package one;
+
+import three.three;
+import two.two;
+
+public class One {
+    public static void main(String[] args) {
+        two two = new two();
+        three three = new three();
+        two.print();
+        three.print();
+    }
+}
